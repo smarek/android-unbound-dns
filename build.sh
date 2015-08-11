f0a3381dc2426da6b2c3d93a20f1e8d998c0fd81
Marek Sebera <marek.sebera@gmail.com>
2015-08-11 14:44:40 +0200

    Fixed bash syntax

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ export CC gcc
 
 cd openssl-1.0.2d/
 mkdir build
-./config no-ssl2 no-ssl3 shared no-zlib no-asm --prefix=(pwd)/build
+./config no-ssl2 no-ssl3 shared no-zlib no-asm --prefix=`pwd`/build
 make -j4 depend
 make -j4
 make -j4 install_sw
@@ -22,7 +22,7 @@ export CC arm-linux-androideabi-gcc
 # Build Expat library (LibExpat)
 cd expat-2.1.0/
 mkdir build
-./configure --prefix=(pwd)/build --with-sysroot=$ANDROID_SYSROOT --host=arm-linux-androideabi
+./configure --prefix=`pwd`/build --with-sysroot=$ANDROID_SYSROOT --host=arm-linux-androideabi
 make -j4
 make -j4 install
 cd ..
@@ -31,13 +31,13 @@ cd ..
 set -x CFLAGS "--sysroot=$ANDROID_SYSROOT -fPIE -march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16"
 cd libevent-2.0.22-stable/
 mkdir build
-./configure --with-sysroot=$ANDROID_SYSROOT --host=arm-linux-androideabi --prefix=(pwd)/build/
+./configure --with-sysroot=$ANDROID_SYSROOT --host=arm-linux-androideabi --prefix=`pwd`/build/
 make -j4
 make -j4 install
 
 # Configure and build Unbound
 cd unbound-1.5.4/
 mkdir build
-./configure --prefix=(pwd)/build --with-sysroot=$ANDROID_NDK_SYSROOT --host=arm-linux-androideabi --with-ssl=(pwd)/../openssl-1.0.2d/build/ --with-libexpat=(pwd)/../expat-2.1.0/build/ --with-libevent=(pwd)/../libevent-2.0.22-stable/build/ --enable-checking --with-pthreads --with-pic --with-run-dir="" --with-pidfile="" --with-chroot-dir="" --with-conf-file=""
+./configure --prefix=`pwd`/build --with-sysroot=$ANDROID_NDK_SYSROOT --host=arm-linux-androideabi --with-ssl=`pwd`/../openssl-1.0.2d/build/ --with-libexpat=`pwd`/../expat-2.1.0/build/ --with-libevent=`pwd`/../libevent-2.0.22-stable/build/ --enable-checking --with-pthreads --with-pic --with-run-dir="" --with-pidfile="" --with-chroot-dir="" --with-conf-file=""
 make -j4
 make -j4 install
