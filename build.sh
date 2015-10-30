f7fe58bb5583e51cdb7c64dce66f865c252dc27f
Marek Sebera <marek.sebera@gmail.com>
2015-10-30 19:15:56 +0100

    removing build directories is now optional on variable CLEAN_BEFORE_BUILD being set or not

--- build.sh
+++ build.sh
@@ -3,14 +3,16 @@
 # setup environment
 source _setenv_android.bash
 
+# clean build folders if env requires it
+test ! -n "$CLEAN_BEFORE_BUILD" || source build_clean.sh
+
 # Update environment variables
 export CC=gcc
 
 # Build OpenSSL library
 
 cd $_OPENSSL_NAME
-rm -rf build
-mkdir build
+test ! -x build || mkdir build
 ./Configure no-ssl2 no-ssl3 no-shared no-zlib no-comp no-hw -fPIC -pie -fpic --install_prefix=`pwd`/build/ --prefix=".//" --openssldir="ssl" android
 make -j4 depend
 make -j4
@@ -22,8 +24,7 @@ export CC=arm-linux-androideabi-gcc
 
 # Build Expat library (LibExpat)
 cd $_EXPAT_NAME
-rm -rf build
-mkdir build
+test ! -x build || mkdir build
 ./configure --prefix=`pwd`/build --with-sysroot=$ANDROID_SYSROOT --host=arm-linux-androideabi --disable-shared
 make -j4
 make -j4 install
@@ -32,8 +33,7 @@ cd ..
 # Build Libevent
 export CFLAGS="--sysroot=$ANDROID_SYSROOT -pie -fPIE -march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16"
 cd $_LIBEVENT_NAME
-rm -rf build
-mkdir build
+test ! -x build || mkdir build
 ./configure --with-sysroot=$ANDROID_SYSROOT --host=arm-linux-androideabi --prefix=`pwd`/build/ --disable-shared
 make -j4
 make -j4 install
@@ -45,8 +45,7 @@ source _setenv_android.bash
 
 # Configure and build Unbound
 cd $_UNBOUND_NAME
-rm -rf build
-mkdir build
+test ! -x build || mkdir build
 ./configure --prefix=`pwd`/build --with-sysroot=$ANDROID_NDK_SYSROOT --host=arm-linux-androideabi --with-ssl=`pwd`/../openssl-1.0.2d/build/ --with-libexpat=`pwd`/../expat-2.1.0/build/ --with-libevent=`pwd`/../libevent-2.0.22-stable/build/ --enable-checking --with-pthreads --with-pic --with-run-dir="." --with-pidfile="unbound.pid" --with-chroot-dir="." --with-conf-file="unbound.conf" --with-rootkey-file="root.key"
 make -j4
 make -j4 install
---
+++ build_clean.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Cleaning build folders\n"
+
+source _setenv_android.bash
+
+test_del ( ) {
+	echo "test_del $1"
+        test ! -e $1 || rm -rf $1
+}
+
+test_del $_OPENSSL_NAME/build
+test_del $_LIBEVENT_NAME/build
+test_del $_EXPAT_NAME/build
+test_del $_UNBOUND_NAME/build
