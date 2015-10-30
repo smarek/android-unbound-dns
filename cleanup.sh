352eb46d417518c7c13e2438651d1745c4d9120b
Marek Sebera <marek.sebera@gmail.com>
2015-10-30 16:46:54 +0100

    Fixed CURL not following redirects and cleanup checking files can be extracted

--- cleanup.sh
+++ cleanup.sh
@@ -3,4 +3,8 @@
 source _setenv_android.bash
 
 rm -rf $_UNBOUND_NAME $_LIBEVENT_NAME $_OPENSSL_NAME $_EXPAT_NAME
-find . -maxdepth 1 -name '*.tar.gz' -exec tar xvf \{\} \;
+
+tar xvf $_UNBOUND_NAME.tar.gz || error "Cannot unpack $_UNBOUND_NAME.tar.gz"
+tar xvf $_LIBEVENT_NAME.tar.gz || error "Cannot unpack $_LIBEVENT_NAME.tar.gz"
+tar xvf $_OPENSSL_NAME.tar.gz || error "Cannot unpack $_OPENSSL_NAME.tar.gz"
+tar xvf $_EXPAT_NAME.tar.gz || error "Cannot unpack $_EXPAT_NAME.tar.gz"
--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
 source _setenv_android.bash
 
-curl "http://kent.dl.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.tar.gz" > $_EXPAT_NAME.tar.gz
-curl "https://www.openssl.org/source/openssl-1.0.2d.tar.gz" > $_OPENSSL_NAME.tar.gz
-curl "https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz" > $_LIBEVENT_NAME.tar.gz
-curl "https://unbound.net/downloads/unbound-1.5.4.tar.gz" > $_UNBOUND_NAME.tar.gz
+curl -L "http://kent.dl.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.tar.gz" -o $_EXPAT_NAME.tar.gz
+curl -L "https://www.openssl.org/source/openssl-1.0.2d.tar.gz" -o $_OPENSSL_NAME.tar.gz
+curl -L "https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz" -o $_LIBEVENT_NAME.tar.gz
+curl -L "https://unbound.net/downloads/unbound-1.5.4.tar.gz" -o $_UNBOUND_NAME.tar.gz
